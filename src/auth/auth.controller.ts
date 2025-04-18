535f61fed2e20e208dcf07efabb709439c00289f
Oumezzine Farah <84034920+farahoumezzine@users.noreply.github.com>
2025-04-18 16:36:01 +0100

    fix controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController { constructor(private authService: AuthService) {} 
+
+    @Post('login')
+    async login(@Body() body) {
+        const user = await this.authService.validateUser(body.username, body.password);
+        if (!user) {
+            throw new Error('Invalid credentials');
+        }
+        return this.authService.login(user);
+    }
+
+    @Post('register')
+    async register(@Body() body) {
+        return this.authService.register(body.username, body.password, body.role);
+    }
+}
