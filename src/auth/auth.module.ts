988be6046a3316dbcb693a082ebe4d44f332d143
Oumezzine Farah <84034920+farahoumezzine@users.noreply.github.com>
2025-04-18 16:32:34 +0100

    add in auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './Jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule, JwtModule.register({
+    secret: 'secretKey',
+    signOptions: { expiresIn: '1d' },
+  }),],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
